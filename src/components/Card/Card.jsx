80d9367eeb00119059ee3343be77e9f38ed7a1de
gonzalo00913 <gonzalo-masa@outlook.com>
2023-07-10 09:49:28 -0300

    cambios responsive

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,28 +5,25 @@ import { getRecipesById } from "../../redux/actions";
 import Style from "../Card/card.module.css";
 
 const Card = ({ recipe, getRecipesById }) => {
-  console.log("Recipe:", recipe); 
+  console.log("Recipe:", recipe);
   const handleRecipeClick = () => {
     getRecipesById(recipe.id);
   };
 
   return (
     <div className={Style.containerCardAll}>
-    <div className={Style.card}>
-      <img className={Style.cardImage} src={recipe.image} alt={recipe.name} />
-      <Link to={`/detail/${recipe.id}`} onClick={handleRecipeClick}>
-      <h2 className={Style.cardName}>{recipe.name}</h2>
-     </Link>
-     {
-     recipe.Diets.map((diet) => <li className={Style.li}>{diet.name}</li>)
-      }
-      <div >
-
+      <div className={Style.card}>
+        <img className={Style.cardImage} src={recipe.image} alt={recipe.name} />
+        <Link to={`/detail/${recipe.id}`} onClick={handleRecipeClick}>
+          <h2 className={Style.cardName}>{recipe.name}</h2>
+        </Link>
+        {recipe.Diets.map((diet) => (
+          <li className={Style.li}>{diet.name}</li>
+        ))}
+        <div></div>
+      </div>
     </div>
-  </div>
-  </div>
   );
 };
 
 export default connect(null, { getRecipesById })(Card);
-
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,37 +1,19 @@
 import { NavLink } from "react-router-dom";
 import Style from "../HomePage/homePage.module.css";
-/* import backgroundImage from "../img/img-home-page.jpg"; */
 import imgLogoInicio from "../img/logo-inicio-page.png";
 import imgIncioFood from "../img/img-inicio-food.jpg";
-const HomePage = () => {
-  /*   const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    width: "100%",
-    height: "100vh",
-  }; */
 
+const HomePage = () => {
   return (
-    /*       <div className={Style.containertitleParrafo}>
-        <h2 className={Style.title}>My Food app</h2>
-        <p className={Style.parrafo}>
-          "Discover a wide variety of delicious recipes that will inspire you to
-          create dishes. In addition, you will be able to explore valuable
-          culinary tips and tricks that will help you hone your gastronomic
-          skills."
-        </p>
-        <NavLink className={Style.boton} to="/home">Enter</NavLink>
-      </div> */
     <div className={Style.containerColor}>
       <div className={Style.containerColorOne}>
         <div className={Style.containerh1Parrafo}>
-          <h1 className={Style.h1}>Food App</h1>
+          <h1 className={Style.h1}>My Food App</h1>
           <p className={Style.parrafo}>
-            "Discover a wide variety of delicious recipes that will inspire you
+            Discover a wide variety of delicious recipes that will inspire you
             to create dishes. In addition, you will be able to explore valuable
             culinary tips and tricks that will help you hone your gastronomic
-            skills."
+            skills.
           </p>
         </div>
 
