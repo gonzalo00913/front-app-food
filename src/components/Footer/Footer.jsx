819e3992ea54a6d0ce9e4c6d88870a8f73c45d36
gonzalo00913 <gonzalo-masa@outlook.com>
2023-09-17 04:20:52 -0300

    css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,19 +14,23 @@ const Footer = () => {
         </p>
       </div>
       <div className={Style.contact}>
+        <a className={Style.p} href="https://portfoliogem.netlify.app/">
+        <i class="fa-solid fa-globe"></i> Portfolio
+        </a>
+
         <a className={Style.l} href="https://www.linkedin.com/in/gonzalo-masa/">
-          <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-linkedin"></i> LinkedIn
         </a>
 
         <a className={Style.g} href="https://github.com/gonzalo00913">
-          <i className="fa-brands fa-github"></i>
+          <i className="fa-brands fa-github"></i> Github
         </a>
         <a
           className={Style.t}
           href="https://twitter.com/GonzaloMsa"
           rel="noreferrer"
         >
-          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-twitter"></i> Twitter
         </a>
       </div>
     </div>
